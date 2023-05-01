a534d26f1fc62c61ce9e7b04899b159c2498d43b
Sam <8408317+samgabrail@users.noreply.github.com>
2023-05-01 15:02:43 +0000

    config ready

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "3.0.1"
+      version = "3.0.2"
     }
   }
 }
@@ -20,4 +20,6 @@ resource "docker_container" "nginx_container" {
     internal = var.internal_port
     external = var.external_port
   }
+  restart = "always"
 }
+
